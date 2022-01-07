027ccda674ec69f924a234b57e121719a1b1f9a8
Chopper1337 <43015696+Chopper1337@users.noreply.github.com>
2022-01-07 16:35:51 +0000

    Init

---
+++ JMG-Exam/Member.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JMG_Exam
+{
+    internal class Member
+    {
+        public string Name { get; set; }
+
+        public Member()
+        {
+
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
